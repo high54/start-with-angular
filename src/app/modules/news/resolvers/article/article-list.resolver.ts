2e6aee71871feec11c037f7b1b4bb51ec7636d64
Julien <julien.bertacco@gmail.com>
2019-04-19 12:00:46 +0200

    Comments

--- src/app/modules/news/resolvers/article/article-list.resolver.ts
+++ src/app/modules/news/resolvers/article/article-list.resolver.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Article } from '../../models/article.interface';
 import { Resolve } from '@angular/router';
+// Rxjs
 import { Observable } from 'rxjs';
+// Services
 import { ArticleService } from '../../services';
-
+// Models
+import { Article } from '../../models/article.interface';
 @Injectable()
 export class ArticleListResolver implements Resolve<Article[]> {
     constructor(
